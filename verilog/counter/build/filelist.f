1335f9c448a89c80972739ea8df825f86a047937
inftyfred <2019107893@qq.com>
2025-11-24 03:36:01 -0800

    src and scripts

--- verilog/counter/build/filelist.f
+++
@@ -1,4 +0,0 @@
-+incdir+/home/infty/ic/verilog/counter/src
-/home/infty/ic/verilog/counter/tb/tb_counter.v
-/home/infty/ic/verilog/counter/src/counter.v
-/home/infty/ic/verilog/counter/src/timescale.v
