63fcc3355320690ce212708ceacf0df3438d292a
inftyfred <2019107893@qq.com>
2025-11-26 00:37:43 -0800

    task io

---
+++ systemverilog/task_io/src/task_io_test.sv
@@ -0,0 +1,14 @@
+module task_io_test();
+
+	task T3(input [7:0] a, b, output bit [7:0] c);
+		c = a + b;
+	endtask
+
+	initial begin
+		reg [7:0] a, b, c;
+		a = 10; b = 11;
+		T3(a, b, c);
+		$display("a=%d, b=%d, c=%d", a, b, c);
+	end
+
+endmodule
