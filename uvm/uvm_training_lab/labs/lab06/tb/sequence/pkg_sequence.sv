a3ccd6dab5cd4e9ea75d34441221bf549cf691a5
inftyfred <2019107893@qq.com>
2026-07-27 06:26:21 -0700

    sequence response

---
+++ uvm/uvm_training_lab/labs/lab06/tb/sequence/pkg_sequence.sv
@@ -0,0 +1,9 @@
+package pkg_sequence;
+  `include "uvm_macros.svh"
+  import uvm_pkg::*;
+  import pkg_config::*;
+  import pkg_transaction::*;
+  import pkg_agent::*;
+
+  `include "sequence/my_sequence.sv"
+endpackage
